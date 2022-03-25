c09adf45128ce5cd735fc88a9b1aeee56f5c9437
jcremerius <jonas.cremerius@student.hpi.de>
2022-03-25 14:16:13 +0100

    Fix linting

--- extractor/helper.py
+++ extractor/helper.py
@@ -135,5 +135,5 @@ default_drg_list = ["Heart Failure",
                     ]
 subject_case_attributes = ["gender", "anchor_age", "anchor_year", "anchor_year_group", "dod"]
 hadm_case_attributes = ["admittime","dischtime","deathtime","admission_type","admission_location",
-                        "discharge_location","insurance","language","marital_status","ethnicity","edregtime",
-                        "edouttime","hospital_expire_flag"]
+                        "discharge_location","insurance","language","marital_status","ethnicity",
+                        "edregtime","edouttime","hospital_expire_flag"]
